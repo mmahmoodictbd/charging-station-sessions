29bb694fbb31bc1da2ed3d6f221a2e951405ff75
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-04-04 22:05:05 +0200

    added utils.

---
+++ src/main/java/com/unloadbrain/assignement/evbox/util/DateUtil.java
@@ -0,0 +1,17 @@
+package com.unloadbrain.assignement.evbox.util;
+
+import java.util.Date;
+
+/**
+ * Provides date related utils
+ */
+public class DateUtil {
+
+    /**
+     * Return current date.
+     */
+    public Date getCurrentDate() {
+        return new Date();
+    }
+
+}
---
+++ src/main/java/com/unloadbrain/assignement/evbox/util/UuidUtil.java
@@ -0,0 +1,11 @@
+package com.unloadbrain.assignement.evbox.util;
+
+import java.util.UUID;
+
+public class UuidUtil {
+
+    public String getRandomUuid() {
+        return UUID.randomUUID().toString();
+    }
+}
+
